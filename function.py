4d52a67edae726489d6da2bf4618e738fc6eaf43
amarnadh-gamer <144234295+amarnadh-gamer@users.noreply.github.com>
2023-11-02 12:29:43 +0530

    Update function.py

--- function.py
+++ function.py
@@ -22,24 +22,3 @@ def modify_global():
     x=99
     print(x)
 modify_global()
-#--example com--#
-#code start#
-
-import random
-def guess_number():
-    return random.randint(1,100)
-target_number=guess_number()
-attempts=0
-while True:
-    user_guess=int(input("guess a number"))
-    attempts+=1
-
-    if user_guess==target_number:
-        print(f"congratualtions!you won the game in {attempts} attempts")
-        break
-    elif user_guess<target_number:
-        print("try a higher number")
-    else:
-        print("try a lower number")
-#--code end--#
-#--code-2--#
